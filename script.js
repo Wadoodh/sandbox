3054191a53b91d585a6a4d180a7050e4fca620dd
Wadood Hussaini <53196769+Wadoodh@users.noreply.github.com>
2022-08-19 16:50:29 -0400

    before changing code to use async await

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-// set global constants
-const IMAGE_PATH = "https://image.tmdb.org/t/p/w500";
-const BACKDROP_PATH = "https://image.tmdb.org/t/p/original";
-const heroLoading = document.getElementById("hero-loading");
-const gridMovieLoading = document.getElementById("loading-movie-grid");
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?api_key=055f6f9bdc1efba5bbbd107a79a2c5cb";
+
+async function getData() {
+  try {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log("error");
+  }
+}
+
+function errorDetected() {
+  return true;
+}
 
 window.addEventListener("DOMContentLoaded", (event) => {
+  // set global constants
+  const IMAGE_PATH = "https://image.tmdb.org/t/p/w500";
+  const BACKDROP_PATH = "https://image.tmdb.org/t/p/original";
+
+  // dom elements
+  const loader = document.getElementById("loader");
+  const hero = document.getElementById("hero");
+  const heroTitle = document.getElementById("hero-title");
+  const heroDescription = document.getElementById("hero-desc");
+  const movieGrid = document.getElementById("movie-grid");
+
+  // initial styles
+  movieGrid.style.opacity = "0%";
+
+  // handle any errors
   const handleError = (response) => {
     if (!response.ok) {
       throw Error(` ${response.status} ${response.statusText}`);
@@ -13,25 +39,27 @@ window.addEventListener("DOMContentLoaded", (event) => {
     }
   };
 
-  fetch("url")
+  fetch(
+    "https://api.themoviedb.org/3/discover/movie?api_key=055f6f9bdc1efba5bbbd107a79a2c5cb"
+  )
     .then(handleError)
     .then((data) => {
-      // get dom elements
-      const hero = document.getElementById("hero");
-      const heroTitle = document.getElementById("hero-title");
-      const heroDesc = document.getElementById("hero-desc");
-      const movieGrid = document.getElementById("movie-grid");
-
-      // set a random featured movie
+      // set a random featured movie for th ehero section
       const randomMovie = Number(
         (Math.random() * data.results.length).toFixed()
       );
       const featuredMovie = data.results[randomMovie];
 
       // set hero elements
-      hero.style.backgroundImage = `url("${BACKDROP_PATH}${featuredMovie.backdrop_path}")`;
-      heroTitle.textContent = featuredMovie.title;
-      heroDesc.textContent = featuredMovie.overview;
+      // hero.style.backgroundImage = `url("${BACKDROP_PATH}${featuredMovie.backdrop_path}")`;
+      const heroImage = document.createElement("img");
+      heroImage.classList.add("hero-image");
+      heroImage.src = BACKDROP_PATH
+        ? `${BACKDROP_PATH}${featuredMovie.backdrop_path}`
+        : null;
+      hero.appendChild(heroImage);
+      heroTitle.innerText = featuredMovie.title;
+      heroDescription.innerText = featuredMovie.overview;
 
       // iterate through data results
       // create img element for each item
@@ -48,9 +76,19 @@ window.addEventListener("DOMContentLoaded", (event) => {
       console.error(err);
     })
     .finally(() => {
-      console.log("done...");
+      setTimeout(() => {
+        loader.style.opacity = "0%";
+
+        loader.classList.toggle("hide");
+
+        movieGrid.style.opacity = "100%";
+      }, 1500);
     });
 });
+
+// movieGrid.style.display = "grid";
+// gridMovieLoading.classList.toggle("hide");
+// gridMovieLoading.style.opacity = "0%";
 
 /* async function getData() {
   const res = await fetch("https://dev--movies.api-for-webflow.autocode.gg/");
