660748849c865a18339adbcf20dc83f96adf0e58
Wadood Hussaini <53196769+Wadoodh@users.noreply.github.com>
2022-08-19 17:32:36 -0400

    changed to async await

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-const API_URL =
-  "https://api.themoviedb.org/3/discover/movie?api_key=055f6f9bdc1efba5bbbd107a79a2c5cb";
-
-async function getData() {
-  try {
-    const response = await fetch(API_URL);
-    const data = await response.json();
-    return data;
-  } catch (err) {
-    console.log("error");
-  }
-}
-
-function errorDetected() {
-  return true;
-}
-
-window.addEventListener("DOMContentLoaded", (event) => {
-  // set global constants
+window.addEventListener("DOMContentLoaded", async () => {
+  // global constants
   const IMAGE_PATH = "https://image.tmdb.org/t/p/w500";
   const BACKDROP_PATH = "https://image.tmdb.org/t/p/original";
+  const API_URL =
+    "https://api.themoviedb.org/3/discover/movie?api_key=055f6f9bdc1efba5bbbd107a79a2c5cb";
+
+  // functions
+  async function getData() {
+    try {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      return data.results;
+    } catch (err) {
+      errorDetected();
+    }
+  }
+
+  function errorDetected() {
+    document.getElementById("load-icon").style.display = "none";
+    document.getElementById("error-msg").style.display = "block";
+  }
+
+  // get data
+  const movies = await getData();
 
   // dom elements
   const loader = document.getElementById("loader");
@@ -26,75 +30,39 @@ window.addEventListener("DOMContentLoaded", (event) => {
   const heroTitle = document.getElementById("hero-title");
   const heroDescription = document.getElementById("hero-desc");
   const movieGrid = document.getElementById("movie-grid");
+  const heroImage = document.createElement("img");
 
   // initial styles
   movieGrid.style.opacity = "0%";
 
-  // handle any errors
-  const handleError = (response) => {
-    if (!response.ok) {
-      throw Error(` ${response.status} ${response.statusText}`);
-    } else {
-      return response.json();
-    }
-  };
+  // set a random featured movie for the hero section
+  const randomMovie = Number((Math.random() * movies?.length || 1).toFixed());
+  const featuredMovie = movies[randomMovie];
 
-  fetch(
-    "https://api.themoviedb.org/3/discover/movie?api_key=055f6f9bdc1efba5bbbd107a79a2c5cb"
-  )
-    .then(handleError)
-    .then((data) => {
-      // set a random featured movie for th ehero section
-      const randomMovie = Number(
-        (Math.random() * data.results.length).toFixed()
-      );
-      const featuredMovie = data.results[randomMovie];
+  // set hero elements
+  heroImage.classList.add("hero-image");
+  heroImage.src = featuredMovie
+    ? `${BACKDROP_PATH}${featuredMovie.backdrop_path}`
+    : null;
+  hero.appendChild(heroImage);
+  heroTitle.innerText = featuredMovie.title;
+  heroDescription.innerText = featuredMovie.overview;
 
-      // set hero elements
-      // hero.style.backgroundImage = `url("${BACKDROP_PATH}${featuredMovie.backdrop_path}")`;
-      const heroImage = document.createElement("img");
-      heroImage.classList.add("hero-image");
-      heroImage.src = BACKDROP_PATH
-        ? `${BACKDROP_PATH}${featuredMovie.backdrop_path}`
-        : null;
-      hero.appendChild(heroImage);
-      heroTitle.innerText = featuredMovie.title;
-      heroDescription.innerText = featuredMovie.overview;
+  // iterate through data results
+  // create img element for each data item
+  // add class to each image (class exists in Webflow)
+  // append each item to movie grid
+  movies.forEach((movie) => {
+    const img = document.createElement("img");
+    img.classList.add("movie-image");
+    img.src = `${IMAGE_PATH}${movie.poster_path}`;
+    movieGrid.appendChild(img);
+  });
 
-      // iterate through data results
-      // create img element for each item
-      // add class from Webflow to each image
-      // append each item to movie grid
-      data.results.forEach((movie) => {
-        const img = document.createElement("img");
-        img.classList.add("movie-image");
-        img.src = `${IMAGE_PATH}${movie.poster_path}`;
-        movieGrid.appendChild(img);
-      });
-    })
-    .catch(function writeError(err) {
-      console.error(err);
-    })
-    .finally(() => {
-      setTimeout(() => {
-        loader.style.opacity = "0%";
-
-        loader.classList.toggle("hide");
-
-        movieGrid.style.opacity = "100%";
-      }, 1500);
-    });
+  // remove loader and show movie grid
+  setTimeout(() => {
+    loader.style.opacity = "0%";
+    loader.classList.toggle("hide");
+    movieGrid.style.opacity = "100%";
+  }, 1500);
 });
-
-// movieGrid.style.display = "grid";
-// gridMovieLoading.classList.toggle("hide");
-// gridMovieLoading.style.opacity = "0%";
-
-/* async function getData() {
-  const res = await fetch("https://dev--movies.api-for-webflow.autocode.gg/");
-  const data = await res.json();
-  console.log(data);
-  return data;
-}
-
-getData(); */
