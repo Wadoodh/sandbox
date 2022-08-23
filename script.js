5d00756e43928cb916a459c49e1c6b17b6a1003d
Wadood Hussaini <53196769+Wadoodh@users.noreply.github.com>
2022-08-23 10:15:28 -0400

    check input validity

--- script.js
+++ script.js
@@ -4,16 +4,23 @@ const formSteps = [...document.querySelectorAll("[data-step]")];
 let currentStep = 0;
 
 form.addEventListener("click", (e) => {
+  let stepIncrementor = 0;
   const next = e.target.classList.contains("next");
   const previous = e.target.classList.contains("previous");
 
   if (!next && !previous) return;
-  if (next) currentStep += 1;
-  if (previous) currentStep -= 1;
+
+  if (next) stepIncrementor += 1;
+  if (previous) stepIncrementor -= 1;
 
   console.log("next or previous clicked");
 
-  showCurrentStep();
+  const inputs = [...formSteps[currentStep].querySelectorAll("input")];
+  const allInputsValid = inputs.every((input) => input.reportValidity());
+  if (allInputsValid) {
+    currentStep += stepIncrementor;
+    showCurrentStep();
+  }
 });
 
 function showCurrentStep() {
