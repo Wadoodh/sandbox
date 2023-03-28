4099af77e0a6152064aae81759f24d8d202725e5
Wadood Hussaini <wadood@webflow.com>
2023-03-28 10:48:12 -0400

    add input and error functions

--- formValidation.js
+++ formValidation.js
@@ -1,5 +1,6 @@
 const fakeSubmitButton = document.querySelector("#fake-submit");
 const websiteInput = document.querySelector("#website");
+const formErrorMsg = document.querySelector("#form-error");
 
 // add to Webflow form in case someone uncovers the form submit button and clicks it
 function preventDefaultSubmit() {
@@ -11,36 +12,57 @@ websiteInput.addEventListener("keydown", (event) => {
   if (key === "enter") getAuditData();
 });
 
+websiteInput.addEventListener("blur", (event) => {
+  const isInputValid = validURL(event.target.value);
+
+  !isInputValid ? showFormError("Invalid input") : removeFormError();
+});
+
 fakeSubmitButton.addEventListener("click", getAuditData);
+
+function showFormError(errorMessage) {
+  formErrorMsg.textContent = errorMessage;
+  formErrorMsg.style.display = "block";
+}
+
+function removeFormError() {
+  formErrorMsg.textContent = "";
+  formErrorMsg.style.display = "none";
+}
 
 function disableFormSubmitButton() {
   fakeSubmitButton.style.pointerEvents = "none";
   fakeSubmitButton.style.opacity = "50%";
 }
 
+function validURL(str) {
+  const pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  ); // fragment locator
+  return !!pattern.test(str);
+}
+
 async function getAuditData() {
-  disableFormSubmitButton();
-
-  function validURL(str) {
-    const pattern = new RegExp(
-      "^(https?:\\/\\/)?" + // protocol
-        "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
-        "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
-        "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
-        "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
-        "(\\#[-a-z\\d_]*)?$",
-      "i"
-    ); // fragment locator
-    return !!pattern.test(str);
-  }
-
   if (websiteInput.value.length === 0) {
     console.log("field cannot be blank");
+    showFormError("field cannot be blank");
     return;
   } else if (!validURL(websiteInput.value)) {
     console.log("enter correct format for website");
+    showFormError(
+      `Website format should be like "www.domain.com" or "domain.com"`
+    );
     return;
   }
+
+  // disable button if validation passed
+  disableFormSubmitButton();
 
   console.log("Loading...");
 
